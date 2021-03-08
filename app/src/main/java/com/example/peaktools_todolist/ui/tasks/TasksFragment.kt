6cfd9a8542e82eed95d864f20d124fb9cfe78ff6
Nelson <ngoncalves@safetonet.com>
2021-03-08 19:14:21 +0000

    Added nav_graph.xml added necessary components on fragments added necessary classes for fragments

---
+++ app/src/main/java/com/example/peaktools_todolist/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.example.peaktools_todolist.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.example.peaktools_todolist.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+}
